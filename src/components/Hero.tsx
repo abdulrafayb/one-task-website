96f15a279a92c29e2d8078b435b6966f2065d04b
abdulrafayb <abdulrafay200066@gmail.com>
2025-05-05 04:49:01 +0500

    on hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,61 @@
+import ArrowWIcon from '../../public/assets/icons/arrow-w.svg';
+import CursorImage from '../../public/assets/images/cursor.png';
+import MessageImage from '../../public/assets/images/message.png';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
 export const Hero = () => {
-  return null;
+  return (
+    <div className='relative bg-black text-white bg-[linear-gradient(to_bottom,#000000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 lg:py-48 overflow-clip'>
+      <div className='absolute bg-black w-[750px] sm:w-[1536px] lg:w-[2400px] h-[375px] sm:h-[768px] lg:h-[1200px] top-[calc(100%-96px)] sm:top-[calc(100%-120px)] lg:top-[calc(100%-216px)] left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000000_82%,#9560EB)] rounded-[100%]'></div>
+
+      <div className='relative container'>
+        <div className='flex justify-center items-center'>
+          <Link
+            href='/'
+            className='inline-flex gap-3 border border-white/30 py-1 px-2 rounded-lg'
+          >
+            <span className='bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FD8FE)] text-transparent bg-clip-text [-webkit-background-clip:text]'>
+              Version 2.0 is here
+            </span>
+            <span className='inline-flex items-center gap-1'>
+              <span>Read More</span>
+              <ArrowWIcon />
+            </span>
+          </Link>
+        </div>
+        <div className='flex justify-center mt-8'>
+          <div className='relative inline-flex'>
+            <Image
+              src={CursorImage}
+              width={200}
+              alt=''
+              className='absolute top-[108px] right-[476px] hidden sm:inline'
+            />
+            <Image
+              src={MessageImage}
+              width={200}
+              alt=''
+              className='absolute top-[56] left-[498px] hidden sm:inline'
+            />
+            <h1 className='inline-flex text-7xl sm:text-9xl font-bold tracking-tighter text-center'>
+              One Task <br /> at a Time
+            </h1>
+          </div>
+        </div>
+        <div className='flex justify-center'>
+          <p className='text-xl max-w-md text-center mt-8'>
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes.
+          </p>
+        </div>
+        <div className='flex justify-center mt-8'>
+          <button className='bg-white text-black font-medium py-3 px-5 rounded-lg'>
+            Get for free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -4,6 +4,7 @@ import echoLogo from '/public/assets/images/echo.png';
 import celestialLogo from '/public/assets/images/celestial.png';
 import pulseLogo from '/public/assets/images/pulse.png';
 import apexLogo from '/public/assets/images/apex.png';
+import Image from 'next/image';
 
 const images = [
   { src: acmeLogo, alt: 'Acme Logo' },
@@ -15,5 +16,25 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className='bg-black text-white py-[72px] sm:py-24'>
+      <div className='container'>
+        <h2 className='text-xl text-white/70 text-center'>
+          Trusted by the world&apos;s most innovative teams
+        </h2>
+        <div className='relative overflow-hidden before:content-[""] before:absolute before:w-5 before:h-full before:top-0 before:left-0 before:bg-[linear-gradient(to_right,#000000,rgb(0,0,0,0))] after:content-[""] after:absolute after:w-5 after:h-full after:top-0 after:right-0 after:bg-[linear-gradient(to_left,#000000,rgb(0,0,0,0))] mt-9'>
+          <div className='flex gap-16'>
+            {images.map(({ src, alt }, index) => (
+              <Image
+                key={index}
+                src={src}
+                alt={alt}
+                className='flex-none w-auto h-8'
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
